0f642612d9b57ddd50ada330c9b7052f8a68ec53
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-05-07 17:16:52 -0700

    Send back a CSV file.

--- server.js
+++ server.js
@@ -10,7 +10,52 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.use(express.static('client'));
 
 app.post('/upload_json', (req, res) => {
-  console.log(req.body)
-  res.send(req.body);
-  res.end();
+  var flatArray = [];
+  var flatString = "";
+
+  var parsed = JSON.parse(req.body.data);
+
+  // Transform a nested object into a flattened string that lists the shared values of 
+  // every key in chronological order.
+
+  // I. — an object
+  // O. — a string that looks like: 
+  // firstName,lastName,county,city,role,sales,\n,Joshie,Wyattson,San Mateo,San Mateo,Broker,1000000,Beth Jr.,Johnson,San Mateo,Pacifica,Manager,2900000,\n, [etc.]
+  // C. — 
+  // E. — 
+  
+  // parsed — OBJECT
+  // req.body.data — STRING
+
+  var getKeys = function(getKeys) {
+    for (var key in getKeys) {
+      if (key !== 'children') {
+        flatString += key + ',';
+      }
+    }
+    flatString += '\n'
+  }
+
+  var getValues = function(needValues) {
+    for (var keys in needValues) {
+      if (typeof needValues[keys] !== "object") {
+        flatString += needValues[keys] + ',';
+      }
+    }
+    flatString += '\n';
+    if (needValues.children.length === 0) {
+      return;
+    } else {
+      for (var i = 0; i < needValues.children.length; i++) {
+        getValues(needValues.children[i]);
+      }
+    }
+  }
+
+  getKeys(parsed);
+  getValues(parsed);
+  
+  res.setHeader('Content-disposition', 'attachment; filename=testing.csv');
+  res.set('Content-Type', 'text/csv');
+  res.status(200).send(flatString);
 });
